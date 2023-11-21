f5190b1e8fac9e15e5ec16684a03420868a2db5e
Student <student@V314-ENT-008.local>
2023-11-21 16:06:36 -0500

    modified wall jump and dash

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,18 +8,19 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
 
-    public float speed = 9;
+    public float speed;
     public float jumpForce = 10;
+    public bool firstJump;
+    public float wallJumpForce = 5;
     public float xInput;
     public float castDist = 0.6f;
     public LayerMask wallLayer;
     public LayerMask groundLayer;
-    public bool firstJump;
-    public float wallJumpForce = 5;
     public bool canMove = true;
-    public bool firstDash = true;
-    public float dashSpeed = 5;
     public bool facingRight = true;
+    public bool firstDash = true;
+    public float dashSpeed = 10;
+    public float drag = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -30,27 +31,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //For visualizing Raycasts
         Debug.DrawRay(transform.position, Vector2.down * castDist, Color.red);
         Debug.DrawRay(transform.position, Vector2.left * castDist, Color.red);
         Debug.DrawRay(transform.position, Vector2.right * castDist, Color.red);
 
-        Debug.Log(wallRight());
-
+        //Movement
         xInput = Input.GetAxis("Horizontal");
         if (canMove)
         {
             transform.Translate(speed * Time.deltaTime * xInput, 0, 0);
+
+            //Checks which way you are facing
+            if (xInput > 0)
+            {
+                facingRight = true;
+            }
+            else if (xInput < 0)
+            {
+                facingRight = false;
+            }
         }
 
-        if (xInput > 0)
-        {
-            facingRight = true;
-        }
-        else if (xInput < 0)
-        {
-            facingRight = false;
-        }
         
+        //Movement refresher
         if (isGrounded())
         {
             canMove = true;
@@ -58,61 +62,77 @@ public class Player : MonoBehaviour
             firstDash = true;
         }
 
-        /*if (xInput > 0)
-        {   
-            Invoke("RunSpeed", 0.5f);
-        }
-        else
-        {
-            speed = 9;
-        }*/
-
-
+        //Jump if press space and is grounded
         if(Input.GetButtonDown("Jump") && isGrounded())
         {
             jump();
         }
 
+        //Double jump if you only jumped once
         if (Input.GetButtonDown("Jump") && firstJump && !isGrounded())
         {
             jump();
             firstJump = false;
         }
 
+        //Wall jump if you are against a wall in the air
         if (Input.GetButtonDown("Jump") && !isGrounded() && (wallRight() || wallLeft()))
         {
             wallJump();
             firstJump = false;
             canMove = false;
+            firstDash = true;
         }
 
+        //Dash if you are in the air
         if (Input.GetKeyDown(KeyCode.LeftShift) && firstDash && !isGrounded())
         {
             dash();
             canMove = false;
+            firstDash = false;
+            
+        }
+
+        //Wall Side if you are against a wall in the air
+        if (!isGrounded() && (wallRight() || wallLeft()))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, - drag);
         }
     }
 
+    //Dash code
     public void dash()
     {
+        rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+
         if (facingRight)
         {
-            rb.velocity = new Vector2(rb.velocity.x + dashSpeed, rb.velocity.y);
+            rb.velocity = new Vector2(dashSpeed, rb.velocity.y);
             
         }
 
         if (!facingRight)
         {
-            rb.velocity = new Vector2(rb.velocity.x - dashSpeed, rb.velocity.y);
+            rb.velocity = new Vector2(-dashSpeed, rb.velocity.y);
         }
+
+        Invoke("dashTimer", 0.2f);
     }
 
+    public void dashTimer()
+    {
+        rb.constraints = RigidbodyConstraints2D.None;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    //Ground Check
     public bool isGrounded()
     {
         bool isGrounded = Physics2D.Raycast(transform.position, Vector2.down, castDist, groundLayer);
         return isGrounded;
     }
 
+    //Wall Check
     public bool wallRight()
     {
         if(Physics2D.Raycast(transform.position, Vector2.right, castDist, wallLayer))
@@ -137,28 +157,26 @@ public class Player : MonoBehaviour
         }
     }
 
-    void RunSpeed()
-    {
-        speed = 7;
-    }
-
+    //Jump Code
     public void jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 
+    //Wall Jump Code
     public void wallJump()
     {
+        drag = 0;
         if (wallRight())
         {
-            rb.velocity = new Vector2(rb.velocity.x - wallJumpForce, jumpForce);
+            rb.velocity = new Vector2(rb.velocity.x - wallJumpForce, (jumpForce / 2));
             facingRight = false;
             
         }
 
         if (wallLeft())
         {
-            rb.velocity = new Vector2(rb.velocity.x + wallJumpForce, jumpForce);
+            rb.velocity = new Vector2(rb.velocity.x + wallJumpForce, jumpForce / 2);
             facingRight = true;
         }
     }
